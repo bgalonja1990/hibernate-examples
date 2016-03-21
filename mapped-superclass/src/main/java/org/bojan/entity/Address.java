41a90361e6af0292a50ce03fa1d498ffb906635c
bgalonja <bgalonja90@gmail.com>
2016-03-21 09:16:53 +0100

    Mapped superclass

---
+++ mapped-superclass/src/main/java/org/bojan/entity/Address.java
@@ -0,0 +1,65 @@
+package org.bojan.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+@Table(name="ADDRESS")
+public class Address {
+	
+	@Id
+	@GeneratedValue(generator="gen")
+	@GenericGenerator(name="gen", strategy = "foreign", parameters={@Parameter(name="property",value="student")})
+	private int id;
+	
+	private String street;
+	
+	private String houseNum;
+
+	private String city;
+	
+	@OneToOne
+	@PrimaryKeyJoinColumn
+	private Student student;
+	
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getHouseNum() {
+		return houseNum;
+	}
+	public void setHouseNum(String houseNum) {
+		this.houseNum = houseNum;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+}
---
+++ mapped-superclass/src/main/java/org/bojan/entity/Student.java
@@ -0,0 +1,66 @@
+package org.bojan.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Table(name="STUDENT")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+	
+	@OneToOne(mappedBy="student")
+	@Cascade(value=CascadeType.SAVE_UPDATE)
+	private Address address;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
+	
+}
